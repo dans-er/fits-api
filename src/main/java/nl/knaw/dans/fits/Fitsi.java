6b34624e290e5084b421738a0db68d668a577395
henk van den berg <ecco@Hor-Vastus.local>
2015-05-30 15:20:22 +0200

    added NLNZ Metadata Extractor config.xml. No NullPointerExceptions anymore from that part of the globe.

--- src/main/java/nl/knaw/dans/fits/Fitsi.java
+++ src/main/java/nl/knaw/dans/fits/Fitsi.java
@@ -1,6 +1,8 @@
 package nl.knaw.dans.fits;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
 import org.jdom.Document;
@@ -9,6 +11,7 @@ import edu.harvard.hul.ois.fits.Fits;
 import edu.harvard.hul.ois.fits.FitsOutput;
 import edu.harvard.hul.ois.fits.exceptions.FitsConfigurationException;
 import edu.harvard.hul.ois.fits.exceptions.FitsException;
+import edu.harvard.hul.ois.fits.tools.Tool;
 
 public class Fitsi
 {
@@ -22,8 +25,7 @@ public class Fitsi
     
     private Fitsi() throws FitsConfigurationException {
         BasicConfigurator.configure();
-        FITS = new Fits(FITS_HOME);
-        
+        FITS = new Fits(FITS_HOME);        
     }
     
     public static Fitsi instance() throws FitsConfigurationException {
@@ -34,8 +36,21 @@ public class Fitsi
     }
     
     public Document getDocument(File file) throws FitsException {
+        return getDocument(file, false);
+    }
+    
+    public Document getDocument(File file, boolean enableStatistics) throws FitsException {
+        Fits.enableStatistics = enableStatistics;
         FitsOutput fout = FITS.examine(file);
         return fout.getFitsXml();
+    }
+    
+    public List<String> getToolInfo() throws Exception {
+        List<String> infoList = new ArrayList<String>();
+        for (Tool tool : FITS.getToolbelt().getTools()) {
+            infoList.add(tool.getToolInfo().print());
+        }
+        return infoList;
     }
     
 
--- src/test/java/nl/knaw/dans/fits/FitsiTest.java
+++ src/test/java/nl/knaw/dans/fits/FitsiTest.java
@@ -1,16 +1,19 @@
 package nl.knaw.dans.fits;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.List;
 
 import org.jdom.Document;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
+import org.jdom.Namespace;
 import org.junit.Test;
 
 public class FitsiTest
 {
+    
+    
     
     @Test
     public void testInstance() throws Exception {
@@ -23,10 +26,22 @@ public class FitsiTest
     public void testGetDocument() throws Exception {
         Fitsi fits = Fitsi.instance();
         Document doc = fits.getDocument(new File("/Users/ecco/git/fits-api/src/test/resources/test-files/DSC00323.jpg"));
-        System.err.println(new XMLOutputter(Format.getPrettyFormat()).outputString(doc));
-        
+        //System.err.println(new XMLOutputter(Format.getPrettyFormat()).outputString(doc)); 
+        Namespace ns = Namespace.getNamespace("http://hul.harvard.edu/ois/xml/ns/fits/fits_output");
+        assertTrue(doc.getRootElement().getChild("identification", ns).getChildren().size() == 1);
+        assertTrue(doc.getRootElement().getChild("fileinfo", ns).getChildren().size() > 0);
+        assertTrue(doc.getRootElement().getChild("filestatus", ns).getChildren().size() > 0);
+        assertTrue(doc.getRootElement().getChild("metadata", ns).getChildren().size() > 0);
     }
     
-    
+    @Test
+    public void testGetToolInfo() throws Exception {
+        Fitsi fits = Fitsi.instance();
+        List<String> infoList = fits.getToolInfo();
+//        for (String info : infoList) {
+//            System.err.println(info);
+//        }
+        assertTrue(infoList.size() > 0);
+    }
 
 }
